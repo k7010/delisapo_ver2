f9f949b7587b9cd46ff33cb928962bc512b50d35
k7010 <103634772+k7010@users.noreply.github.com>
2022-06-17 00:01:12 +0900

    first commit

---
+++ db/migrate/20220616144157_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users, force: :cascade do |t|
+      t.string      :staff_number,    null: false
+      t.string      :email,           null: false
+      t.string      :password_digest, null: false
+      t.string      :remember_token
+      t.timestamps
+    end
+  end
+end
